7b47277708d978b8a2214979185f77ad64ebc9a9
Collins Ojenge <collinsojenge@gmail.com>
2016-04-17 13:01:05 -0400

    Addition of response to text

--- receive_sms.py
+++ receive_sms.py
@@ -15,10 +15,30 @@ def receive_sms():
 
     # The text which was received
     text = request.values.get('Text')
-
+    
     # Print the message
     print 'Text received: %s - From: %s' % (text, from_number)
     return "Text received"
+    
+    resp = plivoxml.Response()
+    body = 'Thank you for your message'
+    params = {
+    'src' : to_number, # Sender's phone number
+    'dst' : from_number, # Receiver's phone Number
+    'callbackUrl': "http://afya.herokuapp.com/report/", # URL that is notified by Plivo when a response is available and to which the response is sent
+    'callbackMethod' : "GET" # The method used to notify the callbackUrl
+    }
+
+    # Message added
+    resp.addMessage(body, **params)
+
+    ret_response = make_response(resp.to_xml())
+    ret_response.headers["Content-type"] = "text/xml"
+
+    # Prints the XML
+    print resp.to_xml()
+    # Returns the XML
+    return ret_response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
