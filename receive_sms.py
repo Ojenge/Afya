6fc9ca9b8d564fdc5b33bb565714f7a717ffa3d9
Collins Ojenge <collinsojenge@gmail.com>
2016-04-17 13:37:33 -0400

    Print delivery

--- receive_sms.py
+++ receive_sms.py
@@ -41,6 +41,26 @@ def receive_sms():
     # Returns the XML
     return ret_response
 
+
+@app.route("/report/", methods=['GET','POST'])
+def report():
+
+    # Sender's phone number
+    from_number = request.values.get('From')
+
+    # Receiver's phone number - Plivo number
+    to_number = request.values.get('To')
+
+    # Status of the message
+    status = request.values.get('Status')
+
+    # Message UUID
+    uuid = request.values.get('MessageUUID')
+
+    # Prints the status and messageuuid
+    print "From : %s To : %s Status : %s MessageUUID : %s" % (from_number, to_number, status,uuid)
+    return "Delivery reported"
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port,debug=True)
