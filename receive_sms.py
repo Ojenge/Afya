ccfa020aba956f877f127936ea0f3be597c7a1ba
Collins Ojenge <collinsojenge@gmail.com>
2016-05-05 18:05:29 -0400

    addition of bebug

--- receive_sms.py
+++ receive_sms.py
@@ -98,7 +98,8 @@ def receive_sms():
                         req = requests.get("https://wsearch.nlm.nih.gov/ws/query", params=payload)
                         tree = ElementTree.fromstring(req.content)
                         rank = tree.find( './/*[@rank="0"]' )
-                        content = rank.find( './/*[@name="FullSummary"]' )
+                        print rank
+                        content = rank.find('.//*[@name="FullSummary"]')
                         content = jinja2.filters.do_striptags(content.text)  
                         body = re.match(r'(?:[^.:;]+[.:;]){4}', content).group()
     except WatsonException as err:
