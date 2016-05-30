5453dcbcdb1fa864e80c0bb41a53fca1e6b74bad
Collins Ojenge <collinsojenge@gmail.com>
2016-05-30 12:29:32 -0400

    Changes to bring back the response

--- receive_sms.py
+++ receive_sms.py
@@ -208,6 +208,9 @@ def receive_sms():
                     print body
             else:
                 body = "Sorry I could not find anything on that. Could you ask another question?"
+            message = Message.query.get(message.id)
+            message.response = body
+            db.session.commit()
     except WatsonException as err:
         print err
     if device == "Afyadevice":
