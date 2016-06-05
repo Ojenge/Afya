f04bd6b1a8be60ab4fd7a772e18bbb7c84f3e813
Collins Ojenge <collinsojenge@gmail.com>
2016-06-05 19:29:35 -0400

    Text message onboarding into one

--- receive_sms.py
+++ receive_sms.py
@@ -133,12 +133,7 @@ def receive_sms():
     if status == 'ask_name':
         user.username = text
         db.session.commit()
-        body = "Great %s, feel free to ask me any health related questions you may have. I'm here to look after your well being. Remember, just like your doctor, the more you interact with me the more I learn about you to keep you healthy." % (text)
-        dialog = Dialog.query.filter_by(name=from_number).order_by(Dialog.id.desc()).first()
-        post_message(text,dialog.dialogid,from_number,body,user.id)
-        ret_response = send_message(device,from_number,to_number, body)
-        return ret_response
-        body = "I specialize in questions such as 'What is malaria?' or 'What are symptoms of malaria?'. By asking me such questions, I can learn what's important to you"
+        body = "Great %s, feel free to ask me any health related questions you may have. I'm here to look after your well being. Remember, just like your doctor, the more you interact with me the more I learn about you to keep you healthy.I specialize in questions such as 'What is malaria?' or 'What are symptoms of malaria?'. By asking me such questions, I can learn what's important to you" % (text)
         dialog = Dialog.query.filter_by(name=from_number).order_by(Dialog.id.desc()).first()
         post_message(text,dialog.dialogid,from_number,body,user.id)
         ret_response = send_message(device,from_number,to_number, body)
@@ -172,7 +167,6 @@ def receive_sms():
             post_message(text,dialog.dialogid,from_number,body,user.id)
             ret_response = send_message(device,from_number,to_number, body)
         else:
-
             pass  
     return ret_response
 
